8e4585a9384552a068951d95c84da5e5e15b273b
TalyssonG <lopestalysson99@gmail.com>
2021-05-23 16:13:28 -0300

    confirmação de compra

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const fields = document.querySelectorAll("[required]")
+function customValidation(event) {
+    const field = event.target
+
+    if (field.validity)
+        field.setCustomValidity("Esse campo é obrigatório")
+}
+
+for (field of fields) {
+    field.addEventListener("invalid", customValidation)
+
+}
